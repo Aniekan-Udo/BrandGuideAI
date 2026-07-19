313ef58931096832fac554281d358a944a7bd6a7
Aniekan Udo <aniekanudo476@gmail.com>
2026-07-19 09:05:29 +0100

    add production frontend origin to CORS

--- main.py
+++ main.py
@@ -44,6 +44,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 origins = [
     "https://brandguard.com",
+    "https://brandmuse.duckdns.org",
     "http://localhost:8000",
     "http://127.0.0.1:8000",
     "http://localhost:3000",
